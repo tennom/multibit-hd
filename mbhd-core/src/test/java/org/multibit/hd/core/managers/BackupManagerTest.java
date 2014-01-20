6514d36db736beab27fe034343c4fdc1d7b0c93f
Jim Burton <jim618@fastmail.co.uk>
2014-01-20 19:34:21 +0000

    more tests

--- mbhd-core/src/test/java/org/multibit/hd/core/managers/BackupManagerTest.java
+++ mbhd-core/src/test/java/org/multibit/hd/core/managers/BackupManagerTest.java
@@ -30,16 +30,6 @@ import java.io.IOException;
 import java.util.List;
 
 public class BackupManagerTest extends TestCase {
-  private static final String TEST_FILE_COPY_AND_ENCRYPT = "testFileCopyAndEncrypt";
-  private static final String TEST_BACKUP_WALLET_UNENCRYPTED = "testBackupWalletUnencrypted";
-  private static final String TEST_BACKUP_WALLET_ENCRYPTED = "testBackupWalletEncrypted";
-
-  private final CharSequence WALLET_PASSWORD = "horatio nelson 123";
-
-  public static final String CIPHER_TESTDATA_DIRECTORY = "cipher";
-  public static final String CIPHER_WALLET_VERSION_0_FILENAME = "qwertyuiop-version-0.wallet.cipher";
-  public static final String CIPHER_WALLET_VERSION_FF_FILENAME = "qwertyuiop-version-ff.wallet.cipher";
-  public static final String CIPHER_WALLET_PASSWORD = "qwertyuiop";
 
   @Before
   @Override
@@ -57,6 +47,7 @@ public class BackupManagerTest extends TestCase {
     SeedPhraseGenerator seedGenerator = new Bip39SeedPhraseGenerator();
     byte[] seed = seedGenerator.convertToSeed(Bip39SeedPhraseGenerator.split(WalletIdTest.SEED_PHRASE_1));
 
+    // Create a wallet
     WalletData walletData = walletManager.createWallet(temporaryWalletParentDirectory.getAbsolutePath(), seed, "password");
 
     // Create a random temporary directory in which to story the cloudBackups
@@ -102,4 +93,3 @@ public class BackupManagerTest extends TestCase {
     assertEquals("Wallet was not roundtripped correctly", walletData.getWallet().getKeys().get(0).toStringWithPrivate(), recreatedWalletData.getWallet().getKeys().get(0).toStringWithPrivate());
   }
 }
-
